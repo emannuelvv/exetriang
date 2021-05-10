ea8ae216b7af3e68f91cc34bb42d845ba8810b90
emannuelvv <54732031+emannuelvv@users.noreply.github.com>
2021-05-10 20:27:56 -0300

    upando git

---
+++ exerTriangulo/ConsoleApp1/Model/Triangulo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Triangulo
+    {
+
+        private double a, b, c;
+
+
+        public Triangulo(double ladoA, double ladoB, double ladoC)
+        {
+
+            a = ladoA;
+            b = ladoB;
+            c = ladoC;
+        }
+            
+        private string EhValido()
+        
+        {
+            string validacao = "";
+            
+
+            if ( a < b + c  && b < a + c && c < a + b)//validação
+            {
+
+               return validacao = "Triangulo OK";
+
+            }
+
+            return validacao = "Triangulo com algum erro";
+        }
+
+        public string TipoTriangulo()
+        {
+            string validacao = "";
+
+
+            if (EhValido() != "Triangulo com algum erro")
+            {
+                                            
+                 if (a == b && a == c)//equi
+                {
+                    return validacao = "Triangulo Equilatero";
+
+                }
+                else if (a != b && b != c && a != c)//esca
+                {
+                    return validacao = "Triangulo Escaleno";
+
+                }
+                else if (a == b || a == c || b == a || b == c || c == a || c == b)//isó
+                {
+                    return validacao = "Triangulo isósceles";
+                }
+
+            }
+            return EhValido();
+        }
+    }
+}
---
+++ exerTriangulo/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Triangulo triangulo = new Triangulo(3, 4, 5);//chamando clase e add valores
+
+            Console.WriteLine(triangulo.TipoTriangulo());
+
+            Console.ReadLine();
+                      
+         }
+    }
+}
